a7f453b2b60303c49e431a3a035301f1b2c1afbd
romiamontero <rmontero2208@gmail.com>
2026-04-13 09:48:07 -0400

    ns

--- alo.py
+++ alo.py
@@ -1,1 +1,19 @@
-print("Alo")
+print("Alo¿")
+
+#Crenado variables
+
+titulo="clima de hoy" #String
+diadelmes=13 #Int
+mes=4 #Int
+temperatura=22.3 #Float
+llueve=False #Boolean
+
+print(titulo)
+print("Temperatura actual: " ,temperatura, "grados")
+print(diadelmes, " - ", mes)
+
+
+if llueve:
+    print("Tiene que llevar paraguas")
+else:
+    print("Puede llevar polera manga corta")
