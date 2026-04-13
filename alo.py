4c41d8877cea3ef6036ad541f5d47d1b9c90938b
romiamontero <rmontero2208@gmail.com>
2026-04-13 10:23:11 -0400

    pin

--- alo.py
+++ alo.py
@@ -1,20 +1,34 @@
-print("13-04-2026")
+# print("13-04-2026")
 
-#Crenado variables
+# #Crenado variables
 
-titulo="Clima de hoy" #String
-diadelmes=13 #Int
-mes=4 #Int
-temperatura=22.3 #Float
-llueve=False #Boolean
+# titulo="Clima de hoy" #String
+# diadelmes=13 #Int
+# mes=4 #Int
+# temperatura=22.3 #Float
+# llueve=False #Boolean
 
-print(titulo)
-print("Temperatura actual: " ,temperatura, "grados")
-print(diadelmes, "-", mes)
+# print(titulo)
+# print("Temperatura actual: " ,temperatura, "grados")
+# print(diadelmes, "-", mes)
 
 
-if llueve:
-    print("Tiene que llevar paraguas")
+# if llueve:
+#     print("Tiene que llevar paraguas")
+# else:
+#     print("Puede llevar polera sin mangas")
+
+# Pedir password y pin
+# Pida al usuario password en palabra que debe ser "temu"
+# ademas pida en pin que debe ser 3435
+# los dos deben estar correctos para acceder al sistema
+
+
+
+pas=input("Ingrese su password: ")
+pin= int(input("Ingrese su pin: "))
+
+if pas == "temu" and pin == 3435:
+    print("correcto")
 else:
-    print("Puede llevar polera sin mangas")
-
+    print("incorrecto")
