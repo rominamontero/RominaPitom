93150b4904823c88e6cd1981d4dcee26788ac73c
rominamontero <rmontero2208@gmail.com>
2026-04-13 11:01:06 -0400

    ejercicio if largo

--- alo.py
+++ alo.py
@@ -25,10 +25,45 @@
 
 
 
-pas=input("Ingrese su password: ")
-pin= int(input("Ingrese su pin: "))
+# pas=input("Ingrese su password: ")
+# pin= int(input("Ingrese su pin: "))
 
-if pas == "temu" and pin == 3435:
-    print("Acceso concedido")
+# if pas == "temu" and pin == 3435:
+#     print("Acceso concedido")
+# else:
+#     print("Algo salio mal")
+
+ingreso=int(input("Ingrese su sueldo: "))
+
+print("1.- Basico")
+print("2.- Medio")
+print("3.- Superior")
+edu=int(input("Ingrese su nivel educacional: "))
+nacionalidad= input("Ingrese nacionalidad (chilena/otra): ")
+
+credito=0
+
+if ingreso > 500000 and ingreso <=1000000:
+    credito=credito+300000
+elif ingreso > 1000001 and ingreso <1500000:
+    credito=credito+650000
+elif ingreso > 1500001:
+    credito=credito+1000000
 else:
-    print("Algo salio mal")
+    print("No tiene sueldo suficiente")
+
+if edu ==1:
+    print("No tiene beneficio por educacion")
+elif edu==2:
+    credito=credito*1,3
+elif edu ==3:
+    credito=credito*1.5
+
+if nacionalidad =="chilena":
+    credito=credito+300000
+else:
+    print("No tiene beneficio por nacionalidad")
+
+print("Su puntaje de credito es: " ,credito)
+
+    
