380f0746480755dd3a5fe9e5df1410f3f16734ec
rominamontero <rmontero2208@gmail.com>
2026-04-22 17:07:25 -0400

    22/04/2026

--- match.py
+++ match.py
@@ -31,51 +31,53 @@
 #     print(f"Su total es: {total}")
 
 
-def suma():
-    num1= int(input("Ingrese el primer numero: "))
-    num2= int(input("Ingrese el segundo numero: "))            
-    print(f"La sume de sus numeros es: {num1+num2}")
-def resta():
-    num1= int(input("Ingrese el primer numero: "))
-    num2= int(input("Ingrese el segundo numero: "))
-    print(f"La resta de sus numeros es: {num1-num2}")
-def multi():
-    num1= int(input("Ingrese el primer numero: "))
-    num2= int(input("Ingrese el segundo numero: "))
-    print(f"La multiplicacion de sus numeros es: {num1*num2}")
-def division():
-    num1= int(input("Ingrese el primer numero: "))
-    num2= int(input("Ingrese el segundo numero: "))
-    print(f"La dividion de sus numeros es: {num1/num2}")
+# def suma():
+#     num1= int(input("Ingrese el primer numero: "))
+#     num2= int(input("Ingrese el segundo numero: "))            
+#     print(f"La sume de sus numeros es: {num1+num2}")
+# def resta():
+#     num1= int(input("Ingrese el primer numero: "))
+#     num2= int(input("Ingrese el segundo numero: "))
+#     print(f"La resta de sus numeros es: {num1-num2}")
+# def multi():
+#     num1= int(input("Ingrese el primer numero: "))
+#     num2= int(input("Ingrese el segundo numero: "))
+#     print(f"La multiplicacion de sus numeros es: {num1*num2}")
+# def division():
+#     num1= int(input("Ingrese el primer numero: "))
+#     num2= int(input("Ingrese el segundo numero: "))
+#     print(f"La dividion de sus numeros es: {num1/num2}")
 
 
 
 
-op=0
+# op=0
 
-while op != 5:
+# while op != 5:
 
-    op=int(input(''' Ingrese una operacion: 
-                1.- Suma
-                2.- Resta
-                3.- Multiplicacion
-                4.- Divivision
-                5.- Salir
-                '''))
+#     op=int(input(''' Ingrese una operacion: 
+#                 1.- Suma
+#                 2.- Resta
+#                 3.- Multiplicacion
+#                 4.- Divivision
+#                 5.- Salir
+#                 '''))
 
-    match op != 5:
-        case 1: 
-            suma()
-        case 2: 
-            resta()
-        case 3:
-            multi()
-        case 4:
-            division() 
-        case 5:
-            print("Saliendo")
-        case _: 
-            print("Opcion invalida")
+#     match op != 5:
+#         case 1: 
+#             suma()
+#         case 2: 
+#             resta()
+#         case 3:
+#             multi()
+#         case 4:
+#             division() 
+#         case 5:
+#             print("Saliendo")
+#         case _: 
+#             print("Opcion invalida")
+
+
 
 # name="romi"
 # def saludo():
@@ -87,3 +89,43 @@ while op != 5:
 # saludo()
 # chao()
 
+def promedio():
+    nota=int(input("Ingrese la cantidad de notas"))
+    suma=0
+
+    for i in range(nota):
+        n=float(input("Ingrese su nota:"))
+        suma=n+suma
+        prom= suma/nota
+    print(f"Su promedio es {prom}")
+
+def iva():
+    n1=int(input("Ingrese el precio: "))
+    print(f"El precio con iva es: {n1*1.19}")
+
+def saludo():
+    nombre=input("Ingrese su nombre:")
+    print (f"Hola, {nombre}. Cmo le baila¿")
+
+
+op=0
+
+while op != 5:
+
+    op=int(input(''' Ingrese una opcion: 
+                1.- Sacar promedio
+                2.- Calcular iva
+                3.- Saludo
+                4.- Salir
+                '''))
+    match op!= 4:
+        case 1:
+            promedio()
+        case 2: 
+            iva()
+        case 3:
+            saludo()
+        case 4:
+            print("Hasta la proximaa")
+        case _:
+            print("Opcion invalida")
